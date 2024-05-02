87b3856c08c961a0cdbcd4db13eb65c874b9d58b
cammylexi <167337765+cammylexi@users.noreply.github.com>
2024-05-02 15:27:35 -0500

    Update ShoppingCart.h

--- ShoppingCart.h
+++ ShoppingCart.h
@@ -1,1 +1,34 @@
+#ifndef SHOPPING_CART_H
+#define SHOPPING_CART_H
 
+#include <string>
+#include <vector>
+#include "ItemToPurchase.h"
+using namespace std;
+
+class ShoppingCart {
+public:
+    ShoppingCart();
+    ShoppingCart(string name, string date);
+   
+    string GetCustomerName() const;
+    string GetDate() const;
+   
+    void AddItem(ItemToPurchase item);
+    void RemoveItem(string name);
+    void ModifyItem(ItemToPurchase item);
+   
+    int GetNumItemsInCart() const;
+    double GetCostOfCart() const;
+   
+    void PrintTotal();
+    void PrintDescriptions();
+
+private:
+    string customerName;
+    string currentDate;
+    vector<ItemToPurchase> cartItems;
+};
+
+#endif
+ 
